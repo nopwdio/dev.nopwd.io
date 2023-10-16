9306b4d48d782b45ce4d48823ea51c990e829ccb
Adrien Pinet <apinet@users.noreply.github.com>
2023-10-16 22:07:47 +0200

    almost ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Nopwd documentation',
+			title: 'Nopwd doc',
 			logo: {
 				light: './src/assets/light.svg',
 				dark: './src/assets/dark.svg',
